485709fe9e18725088cf91ca69342afc8ee887b8
RensvGemert <rensvgemert@gmail.com>
2022-04-07 22:35:25 +0200

    Full Crud implemented

--- src/components/ProductCreate.js
+++ src/components/ProductCreate.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-function ProductCreate() {
-    return ( <>
-        <h1>Product create form</h1>
-    </> );
+export default function ProductCreate() {
+    const [productTitle, setProductTitle] = useState('');
+    const [productDescription, setProductDescription] = useState('');
+    const [productPrice, setProductPrice] = useState('');
+    const [productImageUrl, setProductImageUrl] = useState('');
+
+    const createProduct = () => {
+        axios.post(`http://localhost:8080/api/products/create`, {
+            productTitle,
+            productDescription,
+            productPrice,
+            productImageUrl
+        })
+    }
+
+    return (
+        <div className='ProductCreate'>
+            <Form className='rounded p-5 p-5'>
+                <h1>Create product</h1>
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                <Form.Label>Title</Form.Label>               
+                <Form.Control 
+                    type="text" 
+                    placeholder="Enter title"
+                    autoComplete='off'
+                    required
+                    onChange={(event) => {
+                        setProductTitle(event.target.value);
+                    }}
+                />
+                </Form.Group>
+            
+                <Form.Group className="mb-3" controlId="formBasicDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    placeholder="Description" 
+                    autoComplete='off'
+                    required
+                    onChange={(event) => {
+                        setProductDescription(event.target.value);
+                    }}                    
+                />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPrice">
+                <Form.Label>Price:</Form.Label>
+                <Form.Control 
+                    type='number'
+                    step=".01"
+                    placeholder='0.00'
+                    autoComplete='off'
+                    required
+                    onChange={(event) => {
+                        setProductPrice(event.target.value);
+                    }}
+                />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicImageUrl">
+                <Form.Label>Image Url:</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    placeholder="http://via.placeholder.com/300x300" 
+                    autoComplete='off'
+                    required
+                    onChange={(event) => {
+                        setProductImageUrl(event.target.value);
+                    }}
+                />
+                </Form.Group>
+
+                <Button 
+                    variant='success'
+                    onClick={createProduct}
+                    type="submit">Create            
+                </Button>
+
+                <Link to="/products" style={{marginLeft: 12}}className="btn btn-danger">Cancel</Link>
+            </Form>
+        </div>
+    )
 }
-
-export default ProductCreate;
--- src/components/ProductUpdate.js
+++ src/components/ProductUpdate.js
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-function ProductUpdate() {
-    return ( 
+import {Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+
+const ProductUpdate = () => {
+    const { id } = useParams();
+    const [productTitle, setProductTitle] = useState('');
+    const [productDescription, setProductDescription] = useState('');
+    const [productPrice, setProductPrice] = useState('');
+    const [productImageUrl, setProductImageUrl] = useState('');
+
+    const updateProduct = () => {
+        axios.put(`http://localhost:8080/api/products/update/${id}`, {
+            productTitle,
+            productDescription,
+            productPrice,
+            productImageUrl
+        },  
+        {
+            headers: {"Content-Type": "application/json"}
+        }
+        )  
+    }
+
+    return (
         <>
-        <h1>Product Update Form</h1>
+            <Form className='rounded p-5 p-5'>
+                <h1>Update product</h1>
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                <Form.Label>Title</Form.Label>               
+                <Form.Control 
+                    type="text" 
+                    autoComplete='off'
+                    onChange={(event) => {
+                        setProductTitle(event.target.value);
+                    }}
+                />
+                </Form.Group>
+            
+                <Form.Group className="mb-3" controlId="formBasicDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    autoComplete='off'
+                    onChange={(event) => {
+                        setProductDescription(event.target.value);
+                    }}
+                />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPrice">
+                <Form.Label>Price</Form.Label>
+                <Form.Control 
+                    type="number" 
+                    step=".01"
+                    autoComplete='off'
+                    onChange={(event) => {
+                        setProductPrice(event.target.value);
+                    }}
+                />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicImage">
+                <Form.Label>Image Url</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    autoComplete='off'
+                    onChange={(event) => {
+                        setProductImageUrl(event.target.value);
+                    }}
+                />
+                </Form.Group>
+
+                <Button 
+                    onClick={updateProduct}
+                    variant="success"
+                    to="/products"
+                    type="submit">Update            
+                </Button>
+
+                <Button href="/products" style={{marginLeft: 12}}className="btn btn-danger">Cancel</Button>
+            </Form>
         </>
-     );
+    );
 }
 
 export default ProductUpdate;
