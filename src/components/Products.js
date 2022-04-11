952138253e44a0b122aba3123802316b8c61222a
Rens Gemert <461328@student.fontys.nl>
2022-04-11 17:19:24 +0200

    CardActionArea added to cards for clickable cards

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import { CardActionArea } from '@mui/material';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -32,11 +33,50 @@ const Products = () => {
             productItems.map(product => (
               <div key={product.productId}>
                 <Card sx={{ maxWidth: 400, margin: '50px' }}>
+                  <CardActionArea href={`/product/details/${product.productId}`}>
+                    <CardActions style={{ justifyContent: 'center' }}>
+                      <Button href={`/product/details/${product.productId}`} size="small">Info</Button>
+                      <Button href={`/product/update/${product.productId}`} size="small"><EditIcon /></Button>
+                      <Button href={`/product/delete/${product.productId}`} style={{ color: 'red' }} size="small"><RemoveCircleOutlineIcon /></Button>
+                    </CardActions>
+
+                    <CardMedia
+                      component="img"
+                      image={product.productImageUrl}
+                      alt=""
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div" style={{ height: 100 }}>
+                        {product.productTitle}
+                      </Typography>
+
+                      <Typography variant="body3" color="text.secondary">
+                        {product.productPrice}
+                      </Typography>
+                    </CardContent>
+                    <CardActions style={{ textAlign: 'center', justifyContent: 'center' }}>
+                      <Button size="small">Add to Cart</Button>
+                    </CardActions>
+                  </CardActionArea>
+                </Card>
+              </div>
+            ))
+          }
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className='grid'>
+        {
+          productItems.map(product => (
+            <div key={product.productId}>
+              <Card sx={{ maxWidth: 400, margin: '50px' }}>
+                <CardActionArea href={`/product/details/${product.productId}`}>
 
                   <CardActions style={{ justifyContent: 'center' }}>
                     <Button href={`/product/details/${product.productId}`} size="small">Info</Button>
-                    <Button href={`/product/update/${product.productId}`} size="small"><EditIcon /></Button>
-                    <Button href={`/product/delete/${product.productId}`} style={{ color: 'red' }} size="small"><RemoveCircleOutlineIcon /></Button>
                   </CardActions>
 
                   <CardMedia
@@ -56,43 +96,7 @@ const Products = () => {
                   <CardActions style={{ textAlign: 'center', justifyContent: 'center' }}>
                     <Button size="small">Add to Cart</Button>
                   </CardActions>
-                </Card>
-              </div>
-            ))
-          }
-        </div>
-      </>
-    );
-  }
-  return (
-    <>
-      <div className='grid'>
-        {
-          productItems.map(product => (
-            <div key={product.productId}>
-              <Card sx={{ maxWidth: 400, margin: '50px' }}>
-
-                <CardActions style={{ justifyContent: 'center' }}>
-                  <Button href={`/product/details/${product.productId}`} size="small">Info</Button>
-                </CardActions>
-
-                <CardMedia
-                  component="img"
-                  image={product.productImageUrl}
-                  alt=""
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div" style={{ height: 100 }}>
-                    {product.productTitle}
-                  </Typography>
-
-                  <Typography variant="body3" color="text.secondary">
-                    {product.productPrice}
-                  </Typography>
-                </CardContent>
-                <CardActions style={{ textAlign: 'center', justifyContent: 'center' }}>
-                  <Button size="small">Add to Cart</Button>
-                </CardActions>
+                </CardActionArea>
               </Card>
             </div>
           ))
