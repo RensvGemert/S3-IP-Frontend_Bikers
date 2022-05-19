e5cb15179148ee5d198b2379b5a4d7a5f5ffe4af
RensvGemert <rensvgemert@gmail.com>
2022-05-19 12:25:49 +0200

    Menu switch when logged in or logged out added

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,9 @@ import Button from '@mui/material/Button';
 
 export default function Header() {
 
+  const userId = localStorage.getItem('userId');
+
+  if(userId !== null){
   return (
     <Box>
       <AppBar position="static">
@@ -15,13 +18,33 @@ export default function Header() {
           <Button color='inherit' href='/products'>Products</Button>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           </Typography>
-          <Button color='inherit' href='/signup'>Signup</Button>
-          <Button color='inherit' href='/signin'>Signin</Button>
-          <Button color='inherit' href='/logout'>Logout</Button>
+
+          <Button color='inherit' href='/logout' >Logout</Button>
           <Button color='inherit' href='/profile'>Profile</Button>
 
         </Toolbar>
       </AppBar>
     </Box>
   );
+ }
+
+ if(userId === null){
+ return (
+  <Box>
+    <AppBar position="static">
+      <Toolbar style={{ backgroundColor: '#5c785a' }}>
+        <Typography style={{ textTransform: 'uppercase', paddingRight: '30px' }}>Bikers</Typography>
+        <Button color='inherit' href='/products'>Products</Button>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        </Typography>
+
+        <Button color='inherit' href='/signup'>Signup</Button>
+        <Button color='inherit' href='/signin'>Signin</Button>
+
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
+}
+
 }
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,5 +1,8 @@
 function Logout() {
     localStorage.clear();
+    return (
+        <h1>Successfully logged out</h1>
+    );
 }
 
 export default Logout;
